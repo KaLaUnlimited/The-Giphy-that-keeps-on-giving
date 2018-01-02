3577bb7e25689fcf0ae562b056b70a387d352a34
KaLaUnlimited <KLasode@gmail.com>
2018-01-01 21:43:19 -0500

    app.js fully functional

--- javascript/app.js
+++ javascript/app.js
@@ -1,51 +1,62 @@
-// var topics = [];
+ var topics = ["Rugrats", "Doug", "Animaniacs", "Hey Arnold", "Power Puff Girls"];
 
+ function getGifs() {
 
+     var input = $(this).data("giphy");
+     var api_key = '3K2ZmyEMrXGGyR7EGBGnbti1HZNk2TZL';
+     var url = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&q=' + input + ' cartoon&limit=10';
 
-function getGifs() {
+     $.ajax({
+         url: url
+     }).done(function(res) {
+         console.log(res);
+         var gifs = res.data;
 
-    var input = $(this).data("giphy");
-    var api_key = '3K2ZmyEMrXGGyR7EGBGnbti1HZNk2TZL';
-    var url = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key + '&q=' + input + '&limit=10';
+         $('#giphy-view').empty();
 
-    $.ajax({
-        url: url
-    }).done(function(res) {
-        console.log (res);
-        var gifs = res.data;
+         for (var i = 0; i < gifs.length; i++) {
+             var url_2 = gifs[i].images.downsized.url;
 
-        $('#giphy-view').empty();
+             //  $('#giphy-view').append($('<div>').css('background-image', 'url(' + url2 + ')'));
 
-        for (var i = 0; i < gifs.length; i++) {
-            var url_2 = gifs[i].images.downsized.url;
+             $('#giphy-view').append($('<img>').attr('src', url_2));
 
-          //  $('#giphy-view').append($('<div>').css('background-image', 'url(' + url2 + ')'));
-        	
-        	$('#giphy-view').append($('<img>').attr('src',url_2));
-        
-        }
-    });
-}
+         }
+     });
+ }
 
-//this function is to add and display giph buttons to the 
-function createGiphyButton() {
-    event.preventDefault();
+ //this function is to add and display giph buttons to the 
+ function createGiphyButton() {
+ 	   $("#giphyButtons-view").empty();
+ 	   event.preventDefault();
+     
+     var giphySearch = $("#giphy-input").val().trim();
+     topics.push(giphySearch);
 
-    var giphySearch = $("#giphy-input").val().trim();
-    var giphyButton = $("<button>");
-
-    giphyButton.addClass("myGiphs");
-    giphyButton.attr("data-giphy", giphySearch)
-
-	
-    giphyButton.text(giphySearch);
-    $("#giphyButtons-view").append(giphyButton);
-  //  topics.push(giphySearch);
+     	displayButtons();
     
-}
+ }
+ //this is loop is to go through topics array and display on DOM
 
-$("#add-giphy").on("click", createGiphyButton);
+ function displayButtons(){
+
+ 	 for (var i = 0; i < topics.length; i++) {
+         var giphyButton = $("<button>");
+
+         giphyButton.addClass("myGiphs");
+         giphyButton.attr("data-giphy", topics[i])
+
+         giphyButton.text(topics[i]);
+         $("#giphyButtons-view").append(giphyButton);
+     }
+
+ }
+
+ displayButtons();
 
 
-//this function is to grab the giph from API
-$(document).on('click', '.myGiphs', getGifs);
+ $("#add-giphy").on("click", createGiphyButton);
+
+
+ //this function is to grab the giph from API
+ $(document).on('click', '.myGiphs', getGifs);
